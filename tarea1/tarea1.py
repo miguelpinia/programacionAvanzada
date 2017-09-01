c14130e4505afdcd736f11d80a216d27813b2a58
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-08-31 21:51:12 -0500

    Funciones de validación de cuadros mágicos.

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+
+def constante_magica(n):
+    """Calcula la constante mágica para un número n. La formula para
+    calcular la constante mágica es n * (n ** 2 + 1) / 2."""
+    return n * (n ** 2 + 1) / 2
+
+def verticales(cuadro, constante):
+    """
+    Verifica que para un cuadro mágico, para cada columna, la suma
+    de los elementos del cuadro sea igual al valor de la constante
+    mágica para el cuadro.
+    """
+    for column in range(len(cuadro)):
+        suma_parcial = sum(renglon[column] for renglon in cuadro)
+        if suma_parcial != constante:
+            return False
+    return True
+
+def horizontales(cuadro, constante):
+    """
+    Verifica que para un cuadro mágica, para cada renglón, la suma
+    de los elementos del cuadro sea igual al valor de la constante
+    mágica para el cuadro. Este valor es pasado como parámetro.
+    """
+    for renglon in cuadro:
+        suma_parcial = sum(val for val in renglon)
+        if suma_parcial != constante:
+            return False
+    return True
+
+def diagonales(cuadro, constante):
+    """
+    Verifica que para un cuadro mágico, para cada diagonal, la suma
+    de los elementos del cuadro sea igual al valor de la constante
+    mágica para el cuadro. Este valor es pasado como parámetro.
+    """
+    n = len(cuadro)
+    sum_1 = sum(cuadro[i][i] for i in range(n))
+    sum_2 = sum(cuadro[i][(n - 1) - i] for i in range(n))
+    return sum_1 == constante == sum_2
