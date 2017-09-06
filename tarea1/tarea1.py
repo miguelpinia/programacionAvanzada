a22744d09a001c164e7d3e4c984375a49b0543fc
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-09-06 17:15:10 -0500

    Agrega clase de pila y cola auxiliares.
    Para la implementación del algoritmo de backtracking, es necesario
    tener una pila para almacenar las soluciones parciales y una cola para
    mantener los elementos con los que se opera.

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -1,5 +1,63 @@
 #!/usr/bin/python3
 
+from collections import deque
+
+class Stack(list):
+    """Clase que representa una pila. Dado que hereda a partir de una
+    lista, sólo se implementan los métodos push para agregar al
+    principio de la pila y peek para ver el tope de la misma.
+
+    Ejemplo de uso:
+
+    pila = Stack()
+    pila.push(1)
+    pila.push(2)
+    pila.push(3)
+    print(pila) # [1, 2, 3]
+    pila.pop()
+    print(pila) # [1, 2]"""
+
+    def __init__(self):
+        """Constructor. Inicializa el constructor de la clase padre."""
+        super(Stack, self).__init__()
+
+    def push(self, val):
+        """Inserta un elemento en el tope de la pila."""
+        self.append(val)
+
+    def peek(self):
+        """Devuelve el valor del elemento en el tope de la pila sin
+        eliminarlo de la pila."""
+        return self[-1]
+
+class Queue(deque):
+    """Clase que representa una cola. Dado que hereda a partir de
+    collections.deque, sólo se implementan los métodos enqueue y
+    dequeue para facilitar su operación al insertar y eliminar
+    elementos.
+
+    Ejemplo de uso:
+
+    cola = Queue()
+    cola.enqueue(1)
+    cola.enqueue(2)
+    cola.enqueue(4)
+    print(cola) # deque([1, 2, 4])
+    cola.dequeue()
+    print(cola) # deque([2, 4])"""
+
+    def __init__(self):
+        """Constructor, inicializa el constructor de la clase padre."""
+        super(Queue, self).__init__()
+
+    def enqueue(self, value):
+        """Inserta un elemento al final de la cola."""
+        self.append(value)
+
+    def dequeue(self):
+        """Saca un elemento al principio de la cola."""
+        self.popleft()
+
 def constante_magica(n):
     """Calcula la constante mágica para un número n. La formula para
     calcular la constante mágica es n * (n ** 2 + 1) / 2."""
@@ -47,6 +105,7 @@ def valida_cuadro(cuadro, constante):
     renglones y diagonales sea igual al valor de la constante mágica
     dada como parámetro.
     """
-    return diagonales(cuadro, constante) \
+    return constante_magica(len(cuadro)) == constante \
+        and diagonales(cuadro, constante) \
         and horizontales(cuadro, constante) \
         and verticales(cuadro, constante)
