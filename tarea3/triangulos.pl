5a0f22ffca8e94fd56372d879c53542c121d6db7
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-11-03 06:51:33 -0600

    Da formato a la función de Ackermann.

--- tarea3/triangulos.pl
+++ tarea3/triangulos.pl
@@ -94,8 +94,16 @@ sumamatriz([V_1|Vs1],[V_2|Vs2], [V_sum|V_sums]) :-
 /*
  * EJERCICIO 5
  */
-ackermann(0,N,X) :- X is N+1.
-ackermann(M, 0, X) :- M>0, M1 is M-1, ackermann(M1, 1, X).
-ackermann(M, N, X) :- M>0, N>0, M1 is M-1, N1 is N-1,
-ackermann(M, N1, X1), ackermann(M1, X1, X).
-
+ackermann(0,N,X) :-
+    X is N+1, !.
+ackermann(M, 0, X) :-
+    M>0,
+    M1 is M-1,
+    ackermann(M1, 1, X), !.
+ackermann(M, N, X) :-
+    M>0,
+    N>0,
+    M1 is M-1,
+    N1 is N-1,
+    ackermann(M, N1, X1),
+    ackermann(M1, X1, X), !.
